2d1e7623782224738dbaa429488ca25e1320051f
samark02 <97296526+samark02@users.noreply.github.com>
2022-06-17 12:06:20 +0530

    Update filename.js

--- filename.js
+++ filename.js
@@ -1,17 +1,32 @@
-var a=1;
+
+// Next button
+var a = 1;
+function up() {
+    a += 1;
+    next()
+}
+function down() {
+    a -= 1;
+    next()
+}
 
 function next() {
-    a+=1;
-    if (a==1){
+    if (a == 1) {
+        document.querySelector('#buttondown').disabled = true;
+        document.querySelector('#buttonup').disabled = false;
         document.getElementById("content").style.display = 'block';
         document.getElementById("content2").style.display = 'none';
     }
-    else if (a==2){
+    else if (a == 2) {
+        document.querySelector('#buttondown').disabled = false;
+        document.querySelector('#buttonup').disabled = false;
         document.getElementById("content").style.display = 'none';
         document.getElementById("content2").style.display = 'block';
         document.getElementById("content3").style.display = 'none';
     }
-    else if (a==3){
+    else if (a == 3) {
+        document.querySelector('#buttondown').disabled = false;
+        document.querySelector('#buttonup').disabled = true;
         document.getElementById("content2").style.display = 'none';
         document.getElementById("content3").style.display = 'block';
         // document.getElementById("content4").style.display = 'none';
