34ea468e955502dd6042719bcbe9f1802c02d85d
samark02 <97296526+samark02@users.noreply.github.com>
2022-06-20 14:37:12 +0530

    Update filename.js

--- filename.js
+++ filename.js
@@ -30,3 +30,54 @@ function next() {
         // document.getElementById("content4").style.display = 'none';
     }
 }
+
+// probe button
+function insert() {
+    document.getElementById("insert").style.display = 'none';
+    document.getElementById("remove").style.display = 'block';
+}
+function remove() {
+    document.getElementById("remove").style.display = 'none';
+    document.getElementById("insert").style.display = 'block';
+}
+
+// current slider
+var cslider = document.getElementById("cslider");
+var coutput = document.getElementById("currentvalue");
+coutput.innerHTML = cslider.value;
+
+cslider.oninput = function() {
+  coutput.innerHTML = this.value;
+}
+
+// thickness slider
+var tslider = document.getElementById("tslider");
+var toutput = document.getElementById("thickness");
+toutput.innerHTML = tslider.value;
+
+tslider.oninput = function() {
+  toutput.innerHTML = this.value;
+}
+
+// hall current slider
+var hcslider = document.getElementById("hcslider");
+var hcoutput = document.getElementById("hallcurrent");
+hcoutput.innerHTML = hcslider.value;
+
+hcslider.oninput = function() {
+  hcoutput.innerHTML = this.value;
+}
+
+function Refresh() {
+    window.location = window.location.href;
+}
+
+// show button
+function voltage() {
+    document.getElementById("showvoltage").style.display = 'none';
+    document.getElementById("showcurrent").style.display = 'block';
+}
+function current() {
+    document.getElementById("showcurrent").style.display = 'none';
+    document.getElementById("showvoltage").style.display = 'block';
+}
