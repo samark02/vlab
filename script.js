3791c00dcb44e1b3f30244541b58449719cc7c71
samark02 <97296526+samark02@users.noreply.github.com>
2022-06-20 16:43:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,106 @@
+
+// Next button
+var a = 1;
+function up() {
+    a += 1;
+    next()
+}
+function down() {
+    a -= 1;
+    next()
+}
+
+function next() {
+    if (a == 1) {
+        document.getElementById("buttondown").style.display = 'none';
+        document.getElementById("content").style.display = 'block';
+        document.getElementById("content2").style.display = 'none';
+    }
+    else if (a == 2) {
+        document.getElementById("buttondown").style.display = 'block';
+        document.getElementById("buttonup").style.display = 'block';
+        document.getElementById("content").style.display = 'none';
+        document.getElementById("content2").style.display = 'block';
+        document.getElementById("content3").style.display = 'none';
+    }
+    else if (a == 3) {
+        document.getElementById("buttonup").style.display = 'none';
+        document.getElementById("content2").style.display = 'none';
+        document.getElementById("content3").style.display = 'block';
+        // document.getElementById("content4").style.display = 'none';
+    }
+}
+
+// probe button
+function insert() {
+    document.getElementById("insert").style.display = 'none';
+    document.getElementById("remove").style.display = 'block';
+
+    document.getElementById("cslider").style.opacity = '1';
+    document.getElementById("tslider").style.opacity = '1';
+    document.getElementById("hcslider").style.opacity = '1';
+
+    document.getElementById("cslider").disabled = false;
+    document.getElementById("materials").disabled = false;
+    document.getElementById("tslider").disabled = false;
+    document.getElementById("hcslider").disabled = false;
+    document.getElementById("showvoltage").disabled = false;
+    document.getElementById("showcurrent").disabled = false;
+
+}
+function remove() {
+    document.getElementById("remove").style.display = 'none';
+    document.getElementById("insert").style.display = 'block';
+    
+    document.getElementById("cslider").style.opacity = '0.5';
+    document.getElementById("tslider").style.opacity = '0.5';
+    document.getElementById("hcslider").style.opacity = '0.5';
+
+    document.getElementById("cslider").disabled = true;
+    document.getElementById("materials").disabled = true;
+    document.getElementById("tslider").disabled = true;
+    document.getElementById("hcslider").disabled = true;
+    document.getElementById("showvoltage").disabled = true;
+    document.getElementById("showcurrent").disabled = true;
+}
+
+// current slider
+var cslider = document.getElementById("cslider");
+var coutput = document.getElementById("currentvalue");
+coutput.innerHTML = cslider.value;
+
+cslider.oninput = function () {
+    coutput.innerHTML = this.value;
+}
+
+// thickness slider
+var tslider = document.getElementById("tslider");
+var toutput = document.getElementById("thickness");
+toutput.innerHTML = tslider.value;
+
+tslider.oninput = function () {
+    toutput.innerHTML = this.value;
+}
+
+// hall current slider
+var hcslider = document.getElementById("hcslider");
+var hcoutput = document.getElementById("hallcurrent");
+hcoutput.innerHTML = hcslider.value;
+
+hcslider.oninput = function () {
+    hcoutput.innerHTML = this.value;
+}
+
+function Refresh() {
+    window.location = window.location.href;
+}
+
+// show button
+function voltage() {
+    document.getElementById("showvoltage").style.display = 'none';
+    document.getElementById("showcurrent").style.display = 'block';
+}
+function current() {
+    document.getElementById("showcurrent").style.display = 'none';
+    document.getElementById("showvoltage").style.display = 'block';
+}
